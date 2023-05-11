c5335596cc7728561824d4c10df65c50f507a3a6
hsm0711 <bingo8@nate.com>
2023-05-11 11:20:28 +0900

    logbak을 위한 MDC 추가, sonarlint 처리

--- src/main/java/com/member/filter/JwtFilter.java
+++ src/main/java/com/member/filter/JwtFilter.java
@@ -8,13 +8,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.MDC;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.google.common.net.HttpHeaders;
+import com.member.constants.MemberConstants;
 import com.member.exception.CustomException;
 import com.member.utils.JwtTokenUtil;
 import com.member.utils.MessageUtils;
@@ -25,46 +28,54 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class JwtFilter extends OncePerRequestFilter {
-
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
-		String username = "", token = "";
+		String username = "";
+		String token = "";
+		String userId = "";
 
 		String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) { // Bearer 토큰 파싱
+		String url = request.getRequestURI();
+
+		if (url.matches("^/\\w+/api/.*") && authorizationHeader != null && authorizationHeader.startsWith(MemberConstants.TOKEN_PREFIX)) { // Bearer 토큰 파싱
 			token = authorizationHeader.substring(7); // jwt token 파싱
 			username = JwtTokenUtil.getUsernameFromToken(token); // username 얻어오기
-		} else {
-			filterChain.doFilter(request, response);
-			return;
-		}
-		// 현재 SecurityContextHolder 에 인증객체가 있는지 확인
-		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			userId = JwtTokenUtil.getUserIdFromToken(token);
 
-			// 토큰 유효여부 확인
-			log.info("JWT Filter token = {}", token);
-			if (JwtTokenUtil.isValidToken(token)) {
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-						username, null, List.of(new SimpleGrantedAuthority("USER")));
+			// 현재 SecurityContextHolder 에 인증객체가 있는지 확인
+			if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-				usernamePasswordAuthenticationToken
-						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				// 토큰 유효여부 확인
+				log.debug(">>> JWT Filter token = {}", token);
+				if (Boolean.TRUE.equals(JwtTokenUtil.isValidToken(token))) {
+					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+							username, null, List.of(new SimpleGrantedAuthority("USER")));
 
-				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+					usernamePasswordAuthenticationToken
+					.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+				}
 			}
+
+			parseJwtToken(authorizationHeader);
 		}
 
-		parseJwtToken(authorizationHeader);
-		filterChain.doFilter(request, response);
-
+		try{
+			MDC.put("userId", userId);
+		    filterChain.doFilter(request, response);
+		}finally{
+			MDC.clear();
+		}
 	}
 
 	public Claims parseJwtToken(String authorizationHeader) {
 		validationAuthorizationHeader(authorizationHeader);
 		String token = extractToken(authorizationHeader);
+
 		try {
 			return Jwts.parser().setSigningKey("secretKey").parseClaimsJws(token).getBody();
 		} catch(Exception e) {
@@ -73,13 +84,13 @@ public class JwtFilter extends OncePerRequestFilter {
 	}
 
 	private void validationAuthorizationHeader(String header) {
-		if (header == null || !header.startsWith("Bearer ")) {
+		if (header == null || !header.startsWith(MemberConstants.TOKEN_PREFIX)) {
 			throw new CustomException(MessageUtils.INVALID_TOKEN_BEARER);
 		}
 	}
 
 	private String extractToken(String authorizationHeader) {
-		return authorizationHeader.substring("Bearer ".length());
+		return authorizationHeader.substring(MemberConstants.TOKEN_PREFIX.length());
 	}
 
 }
