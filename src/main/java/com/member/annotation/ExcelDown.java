26b95861447a61b506dc5d2970925f638cf8da2f
hsm0711 <bingo8@nate.com>
2023-05-16 09:13:21 +0900

    Excel down annotation 구성 수정

--- src/main/java/com/member/annotation/ExcelDown.java
+++ src/main/java/com/member/annotation/ExcelDown.java
@@ -1,7 +1,6 @@
 package com.member.annotation;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.TYPE ;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,50 +11,32 @@ import org.apache.poi.ss.usermodel.CellType;
 /**
  * 엑셀 다운로드 시 다운로드 될 Sheet, 컬럼 정보를 포함하는 annotaion
  */
-@Target({TYPE, FIELD})
+@Target(FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExcelDown {
 	/**
-	 * 해당 Sheet의 이름을 설정한다.
-	 *
-	 * scope: Dto Class
-	 * default: Sheet1
-	 */
-	String sheetName() default "Sheet1";
-
-	/**
-	 * 해당 Column의 해더 이름을 설정한다.
-	 *
-	 * scope: Field
+	 * Column의 해더 이름을 설정한다.
 	 */
 	String headerName();
 
 	/**
-	 * 해당 Column의 withd를 설정한다.
-	 *
-	 * scope: Field
+	 * Column의 withd를 설정한다.
 	 */
 	int width() default -1;
 
 	/**
-	 * 해당 Column이 표시될 순서를 설정한다.
+	 * Column이 표시될 순서를 설정한다.
 	 * 순서가 지정되지 않은 경우 order설정 된 Column들 뒤에 VO/DTO에 선언한 순서로 표시된다.
-	 *
-	 * scope: Field
 	 */
 	int order() default Integer.MAX_VALUE;
 
 	/**
-	 * 해당 Cell의 타입을 설정한다.
-	 *
-	 * scope: Field
+	 * Column의 타입을 설정한다.
 	 */
 	CellType cellType();
 
 	/**
 	 * 해당 Cell Data의 Format을 설정한다.
-	 *
-	 * scope: Field
 	 * ex: "#,##", "yyyy-MM-dd"
 	 */
 	String dataFormat() default "";
