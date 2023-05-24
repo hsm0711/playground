eae15aade348f5326cc92e904ee29b1bc9d489dd
hsm0711 <bingo8@nate.com>
2023-05-24 17:34:36 +0900

    spring boot 버전업, SwaggerConfig 수정, Crypto 샘플 controller swagger 추가

--- src/main/java/com/member/api/sample/CryptoController.java
+++ src/main/java/com/member/api/sample/CryptoController.java
@@ -9,12 +9,19 @@ import com.member.api.sample.model.CryptoRequest;
 import com.member.api.sample.model.CryptoResponse;
 import com.member.model.BaseResponse;
 import com.member.utils.CryptoUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
+@Api(tags = "Crypto 샘플 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member/public/sample/crypto")
 public class CryptoController {
+  /*
+   * 암호화 - AES
+   */
+  @ApiOperation(value = "암호화 - AES", notes = "AES 암호화")
   @PostMapping("/encrypt")
   public ResponseEntity<BaseResponse<CryptoResponse>> encrypt(@RequestBody CryptoRequest request) {
     String input = request.getPlainText();
@@ -22,6 +29,10 @@ public class CryptoController {
     return ResponseEntity.ok(new BaseResponse<>(CryptoResponse.builder().inputStr(input).resultStr(CryptoUtil.encrypt(input)).build()));
   }
 
+  /*
+   * 복호화 - AES
+   */
+  @ApiOperation(value = "복호화 - AES", notes = "AED 복호화")
   @PostMapping("/decrypt")
   public ResponseEntity<BaseResponse<CryptoResponse>> decrypt(@RequestBody CryptoRequest request) {
     String input = request.getEncryptedText();
@@ -29,6 +40,10 @@ public class CryptoController {
     return ResponseEntity.ok(new BaseResponse<>(CryptoResponse.builder().inputStr(input).resultStr(CryptoUtil.decrypt(input)).build()));
   }
 
+  /*
+   * 비밀번호 단방향 암호화
+   */
+  @ApiOperation(value = "비밀번호 단방향 암호화", notes = "비밀번호 단방향 암호화")
   @PostMapping("/password")
   public ResponseEntity<BaseResponse<CryptoResponse>> password(@RequestBody CryptoRequest request) {
     String input = request.getPlainText();
@@ -36,6 +51,10 @@ public class CryptoController {
     return ResponseEntity.ok(new BaseResponse<>(CryptoResponse.builder().inputStr(input).resultStr(CryptoUtil.encodePassword(input)).build()));
   }
 
+  /*
+   * 평문, 암호화 비밀번호 비교
+   */
+  @ApiOperation(value = "평문, 암호화 비밀번호 비교", notes = "평문과 암호화된 비밀번호가 동일한 값인지 비교")
   @PostMapping("/password-compare")
   public ResponseEntity<BaseResponse<Boolean>> passwordCompare(@RequestBody CryptoRequest request) {
     return ResponseEntity.ok(new BaseResponse<>(CryptoUtil.comparePassword(request.getPlainText(), request.getEncryptedText())));
--- src/main/java/com/member/config/SwaggerConfig.java
+++ src/main/java/com/member/config/SwaggerConfig.java
@@ -1,10 +1,9 @@
 package com.member.config;
 
 import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import com.google.common.net.HttpHeaders;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,15 +17,12 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-  private static final String AUTHORIZATION = "Authorization";
-
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.OAS_30).useDefaultResponseMessages(true) // Swagger 에서 제공해주는
-                                                                                 // 기본 응답 코드
+    return new Docket(DocumentationType.OAS_30).useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드
         .apiInfo(apiInfo()) // Swagger UI 로 노출할 정보
-        .securityContexts(List.of(this.securityContext())) // SecurityContext 설정
-        .securitySchemes(List.of(this.apiKey())) // ApiKey 설정
+        .securityContexts(List.of(securityContext())) // SecurityContext 설정
+        .securitySchemes(List.of(apiKey())) // ApiKey 설정
         .select().apis(RequestHandlerSelectors.basePackage("com.member")) // api 스펙이 작성되어 있는 패키지
         .paths(PathSelectors.any()) // apis 에 위치하는 API 중 특정 path 를 선택
         .build();
@@ -45,13 +41,15 @@ public class SwaggerConfig {
   private List<SecurityReference> defaultAuth() {
     AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
     AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+
     authorizationScopes[0] = authorizationScope;
-    return List.of(new SecurityReference(AUTHORIZATION, authorizationScopes));
+
+    return List.of(new SecurityReference(HttpHeaders.AUTHORIZATION, authorizationScopes));
   }
 
   // ApiKey 정의
   private ApiKey apiKey() {
-    return new ApiKey(AUTHORIZATION, AUTHORIZATION, "header");
+    return new ApiKey(HttpHeaders.AUTHORIZATION, HttpHeaders.AUTHORIZATION, "header");
   }
 
 }
