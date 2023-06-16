c24af45bdcf32dbf1959e2cd417aa78af2fc4352
hsm0711 <bingo8@nate.com>
2023-06-16 14:56:37 +0900

    XSS 방지 security 설정, lucy 적용, RequestBody XSS 방지 적용

--- src/main/java/com/member/config/WebConfig.java
+++ src/main/java/com/member/config/WebConfig.java
@@ -1,15 +1,44 @@
 package com.member.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  private final ObjectMapper objectMapper;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").allowedMethods(HttpMethod.POST.name(), HttpMethod.GET.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(),
         HttpMethod.OPTIONS.name());
+  }
+
+  @Bean
+  public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+    FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+
+    filterRegistration.setFilter(new XssEscapeServletFilter());
+    filterRegistration.setOrder(1);
+    filterRegistration.addUrlPatterns("/*");
+
+    return filterRegistration;
+  }
+
+  @Bean
+  public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+    ObjectMapper copy = objectMapper.copy();
+
+    copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+
+    return new MappingJackson2HttpMessageConverter(copy);
   }
 }
