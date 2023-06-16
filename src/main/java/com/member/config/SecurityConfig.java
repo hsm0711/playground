10b675a0c31b6a18e37c8221222b2b8816789f0d
hsm0711 <bingo8@nate.com>
2023-06-16 15:24:46 +0900

    오타 수정

--- src/main/java/com/member/config/SecurityConfig.java
+++ src/main/java/com/member/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http.csrf().disable().headers().xssProtection().and().contentSecurityPolicy("script-src 'slef").and().frameOptions().sameOrigin().and()
+    http.csrf().disable().headers().xssProtection().and().contentSecurityPolicy("script-src 'self").and().frameOptions().sameOrigin().and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 X
         .and().formLogin().disable() // 화면 없으니 설정
         .httpBasic().disable().authorizeRequests().antMatchers("/szs/me", "/szs/scrap", "szs/refund").authenticated().anyRequest().permitAll() // 위 3개의 url을 제외한 나머지는 다 접근 가능 설정
