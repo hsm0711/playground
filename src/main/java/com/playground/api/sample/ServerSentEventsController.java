483d203ac97ba54bd91314be7aeb3c6a663b7d8b
hsm0711 <bingo8@nate.com>
2023-06-26 13:30:30 +0900

    springfox -> springdoc 변경

--- src/main/java/com/playground/api/sample/ServerSentEventsController.java
+++ src/main/java/com/playground/api/sample/ServerSentEventsController.java
@@ -12,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import com.playground.api.sample.model.SseDto;
 import com.playground.api.sample.service.ServerSentEventsService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-@Api(tags = "SSE(Server Sent Events)(EventStream) 샘플 API")
+@Tag(name = "sse", description = "SSE(Server Sent Events)(EventStream) 샘플 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/playground/public/sample/sse")
@@ -26,7 +26,7 @@ public class ServerSentEventsController {
   /**
    * sse publish
    */
-  @ApiOperation(value = "sse 메시지 발송", notes = "sse의 연결된 사용자(id)에게 메시지 전송")
+  @Operation(summary = "sse 메시지 발송", description = "sse의 연결된 사용자(id)에게 메시지 전송")
   @PostMapping("/send-message")
   public ResponseEntity<Void> sendMessage(@RequestBody SseDto sseDto) {
     serverSentEventsService.sendMessage(sseDto);
@@ -37,7 +37,7 @@ public class ServerSentEventsController {
   /**
    * sse 연결
    */
-  @ApiOperation(value = "sse 구독", notes = "sse구독")
+  @Operation(summary = "sse 구독", description = "sse구독")
   @GetMapping(value = "/subscribe/{id}", produces = "text/event-stream")
   public SseEmitter subscribe(@PathVariable String id,
       @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
--- src/main/java/com/playground/config/SwaggerConfig.java
+++ src/main/java/com/playground/config/SwaggerConfig.java
@@ -1,55 +1,56 @@
 package com.playground.config;
 
-import java.util.List;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.google.common.net.HttpHeaders;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.web.method.HandlerMethod;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
   @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.OAS_30).useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드
-        .apiInfo(apiInfo()) // Swagger UI 로 노출할 정보
-        .securityContexts(List.of(securityContext())) // SecurityContext 설정
-        .securitySchemes(List.of(apiKey())) // ApiKey 설정
-        .select().apis(RequestHandlerSelectors.basePackage("com.playground")) // api 스펙이 작성되어 있는 패키지
-        .paths(PathSelectors.any()) // apis 에 위치하는 API 중 특정 path 를 선택
-        .build();
-
+  public GroupedOpenApi publicApi() {
+    return GroupedOpenApi.builder().group("playground").pathsToMatch("/playground/**").build();
   }
 
-  public ApiInfo apiInfo() {
-    return new ApiInfoBuilder().title("회원프로젝트").description("회원프로젝트 Swagger").version("0.1").build();
+  @Bean
+  public OpenAPI openAPI() {
+
+    Info info = new Info().version("v1.0.0").title("Playground API").description("playground 프로젝트 API 명세서");
+
+    // SecuritySecheme명
+    String jwtSchemeName = "jwtAuth";
+
+    // API 요청헤더에 인증정보 포함
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+    // SecuritySchemes 등록
+    Components components = new Components().addSecuritySchemes(jwtSchemeName, new SecurityScheme().name(jwtSchemeName).type(SecurityScheme.Type.HTTP) // HTTP 방식
+        .scheme("bearer").bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+    return new OpenAPI().info(info).addSecurityItem(securityRequirement).components(components);
   }
 
-  // JWT SecurityContext 구성
-  private SecurityContext securityContext() {
-    return SecurityContext.builder().securityReferences(defaultAuth()).build();
+  // 전역 설정 필요 시 사용
+  @Bean
+  public OperationCustomizer operationCustomizer() {
+    return (Operation operation, HandlerMethod handlerMethod) -> {
+      Parameter param = new Parameter().in(ParameterIn.HEADER.toString()) // 전역 헤더 설정
+          .schema(new StringSchema()._default("1234567").name("AppID")) // default값 설정
+          .name("AppID").description("TEST AppID").required(true);
+
+      operation.addParametersItem(param);
+
+      return operation;
+    };
   }
-
-  private List<SecurityReference> defaultAuth() {
-    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-
-    authorizationScopes[0] = authorizationScope;
-
-    return List.of(new SecurityReference(HttpHeaders.AUTHORIZATION, authorizationScopes));
-  }
-
-  // ApiKey 정의
-  private ApiKey apiKey() {
-    return new ApiKey(HttpHeaders.AUTHORIZATION, HttpHeaders.AUTHORIZATION, "header");
-  }
-
 }
