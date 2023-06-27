0af0cfab5a6ab07c0a29d0cafac478d607e6ae9f
hsm0711 <bingo8@nate.com>
2023-06-27 13:32:42 +0900

    secretKey 수정

--- src/main/java/com/playground/filter/JwtFilter.java
+++ src/main/java/com/playground/filter/JwtFilter.java
@@ -2,19 +2,18 @@ package com.playground.filter;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.google.common.net.HttpHeaders;
 import com.playground.constants.PlaygroundConstants;
 import com.playground.exception.CustomException;
@@ -25,7 +24,15 @@ import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class JwtFilter extends OncePerRequestFilter {
+  private static String pwd;
+
+  @Value("${CRYPTO_PWD}")
+  synchronized void setPwd(String cryptoPwd) {
+    pwd = cryptoPwd;
+  }
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
@@ -74,7 +81,7 @@ public class JwtFilter extends OncePerRequestFilter {
     String token = extractToken(authorizationHeader);
 
     try {
-      return Jwts.parser().setSigningKey("secretKey").parseClaimsJws(token).getBody();
+      return Jwts.parser().setSigningKey(pwd).parseClaimsJws(token).getBody();
     } catch (Exception e) {
       throw new CustomException(MessageUtils.NOT_VERIFICATION_TOKEN);
     }
